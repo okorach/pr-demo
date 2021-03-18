3e62570e4bfd01afbceab6d3f50662cefed2b7ae
Olivier Korach <olivier.korach@gmail.com>
2021-03-18 17:15:47 +0100

    sync bitbucket

---
+++ person.py
@@ -0,0 +1,17 @@
+class Person:
+
+    def __init__(self, first=None, last=None):
+        self.firstname = first
+        self.lastname = last
+        self.age = 0
+        self.eye_color = None
+        self.nationality = None
+
+    def fullname(self):
+        return "%s %s" % (self.firstname, self.lastname)
+
+    def happy_birthday(self):
+        return "Happy birthday " + self.firstname
+
+    def is_major(self):
+        return self.age > 18
