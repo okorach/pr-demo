109ceabfe99c285e817c97c96c276d94fcee6120
Olivier Korach <olivier.korach@gmail.com>
2021-07-09 17:24:23 +0200

    Fix script

--- comp-dotnet/dotnet-build.sh
+++ comp-dotnet/dotnet-build.sh
@@ -2,4 +2,4 @@
 dotnet sonarscanner begin /k:"demo:github-dotnet" /n:"GitHub project - .Net Core" \
     /d:"sonar.host.url=$SONAR_HOST_URL" /d:"sonar.login=$SONAR_TOKEN"
 dotnet build dotnetcore-sample.sln
-dotnet sonarscanner end /d:"sonar.login=${SONAR_TOKEN}" -Dsonar.qualitygate.wait=true
+dotnet sonarscanner end /d:"sonar.login=${SONAR_TOKEN}"
