587899a317699cdc5c181b3062596c840ae47ef5
Olivier Korach <olivier.korach@gmail.com>
2021-07-10 14:16:58 +0200

    Fix

--- comp-maven/build.sh
+++ comp-maven/build.sh
@@ -4,7 +4,7 @@
 
 mvn clean install -P coverage-per-test \
    -Dmaven.test.failure.ignore=true \
-   sonar:sonar
+   sonar:sonar \
    -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN \
    -Dsonar.exclusions=pom.xml \
    -Dsonar.projectKey="demo:github-comp-maven" -Dsonar.projectName="GitHub project - Maven" \
